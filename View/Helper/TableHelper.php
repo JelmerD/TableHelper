3f6afdd9270d1307683d9b76264c4b9743ab1c97
Jelmer Dröge <jelmer.droge@gmail.com>
2013-11-22 11:27:52 +0100

    Added a fallback method that allows you to easily parse a row with a special value for when you don't have any rows in your body.

--- View/Helper/TableHelper.php
+++ View/Helper/TableHelper.php
@@ -49,6 +49,20 @@ class TableHelper extends AppHelper
      */
     private $__rowCount;
 
+	/**
+	 * Keep track of the column count per TableHelper::row() method call
+	 *
+	 * @var int
+	 */
+	private $__cellCount;
+
+	/**
+	 * Keep track of the amount of head cells
+	 *
+	 * @var bool|int
+	 */
+	private $__headCellCount;
+
 
     /**
      * Initialise the Table
@@ -76,7 +90,10 @@ class TableHelper extends AppHelper
     public function head($columns = array(), $rowOptions = array(), $options = array())
     {
         $this->__head = $columns;
-        return $this->__bodyClose() . $this->Html->tag('thead', $this->__row($columns, $rowOptions, 'th'), $options);
+
+		$head =  $this->__bodyClose() . $this->Html->tag('thead', $this->__row($columns, $rowOptions, 'th'), $options);
+		$this->__headCellCount = $this->__cellCount; #cache the head cell count
+		return $head;
     }
 
     /**
@@ -108,6 +125,37 @@ class TableHelper extends AppHelper
         $this->__rowCount++;
         return $this->body() . $this->__row($columns, $options);
     }
+
+	/**
+	 * Set a fallback for when the rowCount is equal to 0, there are two ways you could use this method
+	 *
+	 * - You can fill the $columns with a string, which will just be one cell, but with an automatick colspan value that equals the amount of cell in your head() method, or if you didn't use the head() method, the last cellCount (could be 0!)
+	 *
+	 * - You can fill the $columns with an array, just like as when you would use row(), but then there won't be an automatic colspan value
+	 *
+	 * <b>IMPORTANT! Make sure to parse this after the TableHelper::row() methods, but before the TableHelper::foot() method</b>
+	 *
+	 * @param string|array $columns Could either be an array of cells, or a string with an automatic colspan of the active columns
+	 * @param array $options
+	 *
+	 * @return null|string
+	 */
+	public function fallback($columns, $options = array()){
+		if ($this->get('rowCount') === 0){
+
+			# if it is a string or numeric value, add a colspan value that matches the "width" of the table
+			if (!is_array($columns)){
+				$columns = array(array(
+					$columns, array(
+						'colspan' => $this->__headCellCount === false ? $this->__cellCount : $this->__headCellCount
+					)
+				));
+			}
+
+			return $this->body() . $this->__row($columns, $options);
+		}
+		return null;
+	}
 
     /**
      * Parse the `tfoot` section (and close the `tbody` section)
@@ -142,13 +190,20 @@ class TableHelper extends AppHelper
      * @param string|array $value The value to have in this cell
      * @param null $key The key used for this cell, if the same key in the `thead` section is `false`, it won't be parsed
      * @param string $tag The tag to use (either `td` or `th`)
+	 * @param bool $count Set to true if you want this cell to be added to the "getLastCellCount" result
      * @return null|string The complete cell, or null if the value at the same key in `thead` is set to `false`
      */
-    public function cell($value, $key = null, $tag = 'td')
+    public function cell($value, $key = null, $tag = 'td', $count = false)
     {
         if (isset($this->__head[$key]) && $this->__head[$key] === false) {
             return null;
         }
+
+		# if the count has to be incremented, increment
+		if ($count){
+			$this->__cellCount++;
+		}
+
         # if the value is an array, the user could have parsed a content and options key. Gives the user extra control.
         return $this->Html->tag(
             $tag,
@@ -156,6 +211,32 @@ class TableHelper extends AppHelper
             is_array($value) && array_key_exists(1, $value) ? $value[1] : array()
         );
     }
+
+	/**
+	 * Get a variable from the cache that could be useful to you
+	 *
+	 *
+	 * <b>Available to you:</b>
+	 * <li>head (a cache of the $columns parsed in head())</li>
+	 * <li>bodyOpen (a boolean that indicates wether the tbody tag is open or not</li>
+	 * <li>rowCount (the amount of rows currently parsed via row())</li>
+	 * <li>cellCount (the amount of cells parsed in the last head(), row() or foot() method)</li>
+	 * <li>headCellCount (the amount of cells parsed in the head() method, false if head() is not used)</li>
+	 *
+	 * @param $variable
+	 *
+	 * @return mixed
+	 */
+	public function get($variable){
+		return $this->{'__' . $variable};
+	}
+
+	/**
+	 * Reset the cellCount to 0
+	 */
+	public function resetCellCount(){
+		$this->__cellCount = 0;
+	}
 
     /**
      * Parse a row with a certain cell tag
@@ -167,9 +248,10 @@ class TableHelper extends AppHelper
      */
     private function __row($columns = array(), $options = array(), $tag = 'td')
     {
-        $cells = null;
+        $this->resetCellCount();
+		$cells = null;
         foreach ($columns as $key => $value) {
-            $cells .= $this->cell($value, $key, $tag);
+            $cells .= $this->cell($value, $key, $tag, true);
         }
         return $this->Html->tag('tr', $cells, $options);
     }
@@ -196,6 +278,8 @@ class TableHelper extends AppHelper
         $this->__head = array();
         $this->__bodyOpen = false;
         $this->__rowCount = 0;
+        $this->resetCellCount();
+		$this->__headCellCount = false;
     }
 
 }
